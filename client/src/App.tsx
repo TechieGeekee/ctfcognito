ccd2fb4081e89c981238757b9c27267cde7ab150
TechieGeekee <164867437+TechieGeekee@users.noreply.github.com>
2025-09-29 10:36:20 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import ImagePreloader from "@/components/image-preloader";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/home";
 
@@ -16,11 +18,15 @@ function Router() {
 }
 
 function App() {
+  const [imagesLoaded, setImagesLoaded] = useState(false);
+
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
-        <Toaster />
-        <Router />
+        <ImagePreloader onComplete={() => setImagesLoaded(true)}>
+          <Toaster />
+          <Router />
+        </ImagePreloader>
       </TooltipProvider>
     </QueryClientProvider>
   );
